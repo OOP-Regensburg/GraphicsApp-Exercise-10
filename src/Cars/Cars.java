ad26149765145687530a6d54a6a4053ad5dee8f4
FS <florin.schwappach@ur.de>
2020-01-07 17:25:26 +0100

    Added modified car code to solution

--- src/Cars/Cars.java
+++ src/Cars/Cars.java
@@ -4,6 +4,8 @@ import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
 
+import java.util.ArrayList;
+
 public class Cars extends GraphicsApp {
 
     /* Private Konstanten */
@@ -11,6 +13,12 @@ public class Cars extends GraphicsApp {
     private static final int CANVAS_WIDTH = 800;
     private static final int FRAME_RATE = 60;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
+
+    private static final int CAR_NUM = 100;
+    private static final int CAR_WIDTH = 15;
+    private static final int CAR_HEIGHT = 5;
+
+    private ArrayList<Car> cars;
 
     /*
      * Die initialize-Methode wird einmalig zum Start des Programms
@@ -20,6 +28,7 @@ public class Cars extends GraphicsApp {
     @Override
     public void initialize() {
         setupCanvas();
+        setupCars();
     }
 
     /*
@@ -30,10 +39,26 @@ public class Cars extends GraphicsApp {
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
+        drawCars();
     }
 
     private void setupCanvas() {
         setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
         setFrameRate(FRAME_RATE);
+    }
+
+    private void drawCars() {
+        for(Car car: cars) {
+            car.update();
+            car.draw();
+        }
+    }
+
+    private void setupCars() {
+        cars = new ArrayList<Car>();
+        for (int i = 0; i < CAR_NUM; i++) {
+            Car car = new Car(CAR_WIDTH, CAR_HEIGHT, CANVAS_WIDTH, CANVAS_HEIGHT);
+            cars.add(car);
+        }
     }
 }
